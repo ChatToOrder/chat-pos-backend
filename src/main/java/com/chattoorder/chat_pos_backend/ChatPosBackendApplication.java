36858fb5142291d400a352e971af0fe41f681f76
Shubh <shubhmukeshagarwal@gmail.com>
2026-02-12 00:55:49 +0530

    chore: initialize spring boot backend project structure

---
+++ src/main/java/com/chattoorder/chat_pos_backend/ChatPosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.chattoorder.chat_pos_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatPosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatPosBackendApplication.class, args);
+	}
+
+}
